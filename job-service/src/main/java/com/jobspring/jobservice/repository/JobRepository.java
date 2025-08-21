43d383f468f9eaf7ba897130570b6a8daf059f1d
Siyam-Bhuiyan <bhuiyansiyam294@gmail.com>
2025-08-21 19:59:32 +0600

    conflict solved

--- job-service/src/main/java/com/jobspring/jobservice/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobspring.jobservice.repository;
-
-import com.jobspring.jobservice.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
