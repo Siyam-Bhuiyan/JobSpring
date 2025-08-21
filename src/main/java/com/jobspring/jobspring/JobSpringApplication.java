28a149f2210eb0a3e9826f156dd5abbf82f8929c
Siyam-Bhuiyan <bhuiyansiyam294@gmail.com>
2025-08-21 20:47:14 +0600

    database connected

--- src/main/java/com/jobspring/jobspring/JobSpringApplication.java
+++ src/main/java/com/jobspring/jobspring/JobSpringApplication.java
@@ -1,13 +1,13 @@
-package com.jobspring.userservice;
+package com.jobspring.jobspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class JobSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(JobSpringApplication.class, args);
     }
 
 }
--- src/test/java/com/jobspring/jobspring/JobSpringApplicationTests.java
+++ src/test/java/com/jobspring/jobspring/JobSpringApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jobspring.userservice;
+package com.jobspring.jobspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class JobSpringApplicationTests {
 
     @Test
     void contextLoads() {
