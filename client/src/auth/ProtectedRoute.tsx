c32047fff1a4dd1397889bb139e5834a861038fb
Siyam-Bhuiyan <bhuiyansiyam294@gmail.com>
2025-09-03 10:04:02 +0600

    routing fixed

--- client/src/auth/ProtectedRoute.tsx
+++ client/src/auth/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./useAuth";
+import { useAuth } from "./hooks";
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
