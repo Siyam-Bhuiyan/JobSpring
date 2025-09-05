6aeb8bbc8173a353f098839a5ac99f40956092d2
Sadia Afrin <afrinzzxx@gmail.com>
2025-09-06 03:13:28 +0600

    Settings DropDown

--- client/src/Components/chatBot/ChatBot.tsx
+++ client/src/Components/chatBot/ChatBot.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { Send } from "lucide-react";
 
@@ -26,7 +26,8 @@ const ChatBot = () => {
   };
 
   return (
-    <div className="fixed bottom-16 right-24 center w-80 shadow-xl rounded-2xl overflow-hidden bg-mine-shaft-900 text-white flex-1 flex-col border border-mine-shaft-700">
+    <div className="p-10 flex justify-center items-end">
+    <div className=" bottom-16 right-10center shadow-xl rounded-2xl overflow-hidden bg-mine-shaft-900 text-white w-2/5 border border-mine-shaft-700">
       {/* Header */}
       <div className="bg-green-600 p-4 font-semibold">ChatBot</div>
 
@@ -38,7 +39,7 @@ const ChatBot = () => {
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.2 }}
-            className={`max-w-[75%] p-3 rounded-lg ${
+            className={`max-w-[40%] p-3 rounded-lg ${
               msg.sender === "user"
                 ? "ml-auto bg-green-500 text-white"
                 : "bg-mine-shaft-700 text-gray-200"
@@ -66,6 +67,7 @@ const ChatBot = () => {
           <Send size={18} />
         </button>
       </div>
+    </div>
     </div>
   );
 };
